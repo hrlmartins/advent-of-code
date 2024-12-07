604e3c643d11cc6d4829ba19a7c4a38e38dc7faa
hrlmartins <4127511+hrlmartins@users.noreply.github.com>
2024-12-07 11:53:51 +0000

    small fixes

--- 2024/07/main.py
+++ 2024/07/main.py
@@ -1,7 +1,7 @@
-from operator import add, mul
-from itertools import product
-from re import A, I
 import sys
+from functools import reduce
+from itertools import product
+from operator import add, mul
 from typing import Any
 
 
@@ -11,21 +11,29 @@ def parse_input(input_data: str) -> list[str]:
     """
     return [line.strip() for line in input_data.strip().splitlines()]
 
+
 def produce_list_tests(data):
-    return [(int(raw.split(":")[0]),list(map(int, raw.split(":")[1].split()))) for raw in data]
+    return [
+        (int(raw.split(":")[0]), list(map(int, raw.split(":")[1].split())))
+        for raw in data
+    ]
+
 
 def generate_combinations(operators, size):
     return list(product(operators, repeat=size))
 
+
 def join_integers(first, second):
     return int(str(first) + str(second))
 
-def apply_operation(operands, operators, fun_map):
-    result = operands[0]
-    for idx, operator in enumerate(operators):
-        result = fun_map[operator](result, operands[idx + 1])
 
-    return result
+def apply_operation(operands, operators, fun_map):
+    return reduce(
+        lambda acc, pair: fun_map[pair[1]](acc, operands[pair[0] + 1]),
+        enumerate(operators),
+        operands[0],
+    )
+
 
 def produce_set_of_matching_tests(test_list, operators, fun_map):
     return {
@@ -39,44 +47,22 @@ def produce_set_of_matching_tests(test_list, operators, fun_map):
 def part1(data: list[str]) -> Any:
     test_raw = produce_list_tests(data)
     operators = ["+", "*"]
-    fun_map = {
-        "+": add,
-        "*": mul
-    }
+    fun_map = {"+": add, "*": mul}
 
-    sum_set = set()
-    for expected, operands in test_raw:
-        operators_combinations = generate_combinations(operators, len(operands) - 1)
-        for combination in operators_combinations:
-            result = operands[0]
-            for idx, operator in enumerate(combination):
-                result = fun_map[operator](result, operands[idx + 1])
-            if result == expected:
-                sum_set.add(expected)
-
-    return sum(value for value in sum_set)
+    return sum(
+        value for value in produce_set_of_matching_tests(test_raw, operators, fun_map)
+    )
 
 
 def part2(data: list[str]) -> Any:
     test_raw = produce_list_tests(data)
     operators = ["+", "*", "||"]
-    fun_map = {
-        "+": add,
-        "*": mul,
-        "||": join_integers
-    }
+    fun_map = {"+": add, "*": mul, "||": join_integers}
 
-    sum_set = set()
-    for expected, operands in test_raw:
-        operators_combinations = generate_combinations(operators, len(operands) - 1)
-        for combination in operators_combinations:
-            result = operands[0]
-            for idx, operator in enumerate(combination):
-                result = fun_map[operator](result, operands[idx + 1])
-            if result == expected:
-                sum_set.add(expected)
+    return sum(
+        value for value in produce_set_of_matching_tests(test_raw, operators, fun_map)
+    )
 
-    return sum(value for value in sum_set)
 
 def main() -> None:
     input_data = sys.stdin.read()
